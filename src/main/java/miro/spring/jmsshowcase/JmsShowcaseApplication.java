b81dc76a233849b3063431b0b3735e711c7bf141
Miroslav Moguš <miromogus@gmail.com>
2019-09-13 09:45:05 +0200

    Initial project commit

---
+++ src/main/java/miro/spring/jmsshowcase/JmsShowcaseApplication.java
@@ -0,0 +1,13 @@
+package miro.spring.jmsshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsShowcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miro/spring/jmsshowcase/JmsShowcaseApplicationTests.java
@@ -0,0 +1,16 @@
+package miro.spring.jmsshowcase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmsShowcaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
